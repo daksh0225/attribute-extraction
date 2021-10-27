b3ab738b5de19ea27f503fafa951962269914b0a
Daksh Rawat <daksh0225@gmail.com>
2021-10-27 20:12:51 +0530

    added code for coref resolution

---
+++ coref_resolution/coref_resolve.py
@@ -0,0 +1,24 @@
+import spacy
+import neuralcoref
+from tqdm import tqdm
+import json
+import sys
+
+nlp = spacy.load("en_core_web_sm")
+neuralcoref.add_to_pipe(nlp)
+category = sys.argv[2]
+
+file = open(sys.argv[1]+"/"+category+"_data.json")
+json_data = json.load(file)
+file.close()
+
+count = 0
+
+for i in tqdm(range(len(json_data))):
+	doc = nlp(json_data[i]["processed_text"])
+	json_data[i]["coref_resolved"] = doc._.coref_resolved
+	if count%100 == 0:
+		file = open("./coref_resolved/"+category+"_coref_resolved_data.json", "w+")
+		json.dump(json_data, file, indent=4)
+		file.close()
+	count += 1
